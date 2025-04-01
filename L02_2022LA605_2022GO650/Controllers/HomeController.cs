c53a984220b1083735022969408e302ad59859ae
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-04-01 16:50:44 -0600

    Agregar archivos de proyecto.

---
+++ L02_2022LA605_2022GO650/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using L02_2022LA605_2022GO650.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace L02_2022LA605_2022GO650.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ L02_2022LA605_2022GO650/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L02_2022LA605_2022GO650.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
