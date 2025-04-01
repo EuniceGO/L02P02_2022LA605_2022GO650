3f2d6ebe6f868534e2c8d140d1d2618b78d9b13e
Katherine Gutierrez <katherine.gutierrez@catolica.edu.sv>
2025-04-01 17:09:10 -0600

    Agregando Context

---
+++ L02_2022LA605_2022GO650/Models/libreriaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L02P02_2022LA605_2022GO650.Models
+{
+    public class libreriaDbContext : DbContext
+    {
+        public libreriaDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- L02_2022LA605_2022GO650/Program.cs
+++ L02_2022LA605_2022GO650/Program.cs
@@ -1,9 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using L02_2022LA605_2022GO650.Models;
+using L02P02_2022LA605_2022GO650.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options => {
+    options.IdleTimeout = TimeSpan.FromSeconds(3600);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddDbContext<libreriaDbContext>(opt =>
+        opt.UseSqlServer(
+            builder.Configuration.GetConnectionString("equipoDbConnection")));
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -19,6 +34,8 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
